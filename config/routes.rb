e6559cc4b39594cf016e4547c22e3d044462e58b
Zachary Hickson <zacharyh96@gmail.com>
2014-04-08 12:48:51 +1000

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Kneel::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
   devise_for :users
   resources :lists
 
