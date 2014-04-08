ce5064659a0e81eaa16431920d25810efd860c83
Zachary Hickson <zacharyh96@gmail.com>
2014-04-08 15:29:20 +1000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Kneel::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   devise_for :users
   resources :lists
 
